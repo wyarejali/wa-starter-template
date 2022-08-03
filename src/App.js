c0c7c8c7273da6fe46ddf5137620efe75642e45a
Wyarej Ali <wyarejali@gmail.com>
2022-08-03 13:09:22 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='app-github-users'>
+      <div className='container'>
+        <h1>GitHub Users</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import Container from './layouts/Container'
+
+ReactDOM.render(
+  <Container>
+    <App />
+  </Container>,
+  document.getElementById('root')
+)
